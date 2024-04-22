2f7379992b5b3a1dc7a2e093baeb915cf8500de9
Ruben Pinazo Robles <rpinazo-@c2r6s6.42malaga.com>
2024-04-22 19:56:18 +0200

    subida substr con errores

--- Cursus/Libft/Funciones_Libc/libft.h
+++ Cursus/Libft/Funciones_Libc/libft.h
@@ -6,7 +6,7 @@
 /*   By: rpinazo- <rpinazo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:46:47 by rpinazo-          #+#    #+#             */
-/*   Updated: 2024/04/22 18:12:56 by rpinazo-         ###   ########.fr       */
+/*   Updated: 2024/04/22 19:21:56 by rpinazo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
