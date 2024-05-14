c28b828f8b666470e57cc5773cb16aa3cc664020
rubenpr13 <rubenpinazorobles@gmail.com>
2024-05-14 12:43:15 +0200

    subida adicionales, menos itoa y split

---
+++ Cursus/Libft/Funciones_Libc/ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpinazo- <rpinazo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 11:16:52 by rpinazo-          #+#    #+#             */
+/*   Updated: 2024/05/14 11:16:52 by rpinazo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	char	*temp;
+	size_t	i;
+
+	temp = malloc(ft_strlen(s) * sizeof(char));
+	i = 0;
+	if (!temp)
+		return (NULL);
+	while (i < ft_strlen(s))
+	{
+		temp[i] = (*f)(i, s[i]);
+		i++;
+	}
+	temp[i] = 0;
+	return (temp);
+}
