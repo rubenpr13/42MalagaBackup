b8cc7f644f3ea5fd5114a9428c988019431bc6aa
rubenpr13 <rubenpinazorobles@gmail.com>
2024-06-13 18:17:32 +0200

    subida itoa y split

--- Cursus/Libft/Funciones_Libc/ft_itoa.c
+++ Cursus/Libft/Funciones_Libc/ft_itoa.c
@@ -14,5 +14,51 @@
 
 char	*ft_itoa(int n)
 {
-	
+	int 	i;
+	int 	isNegative;
+	char	*str;
+	char	temp;
+
+	i = 0;
+	isNegative = 0;
+	if (n == 0)
+	{
+		str[i++] = '0';
+		str[i] = '\0';
+		return (str);
+	}
+	if (n < 0)
+	{
+		isNegative = 1;
+		n = -n;
+	}
+	while (n != 0)
+	{
+		temp = n % 10;
+		str[i++] = temp + '0';
+		n = n / 10;
+	}
+	if (isNegative)
+		str[i++] = '-';
+	str[i] = '\0';
+	reverse(str, i);
+	return (str);
+}
+
+static void	reverse(char *str, int lenght)
+{
+	int		start;
+	int		end;
+	char	temp;
+
+	start = 0;
+	end = lenght - 1;
+	while (start < end)
+	{
+		temp = str[start];
+		str[start] = str[end];
+		str[end] = temp;
+		start++;
+		end--;
+	}
 }
--- Cursus/Libft/Funciones_Libc/ft_split.c
+++ Cursus/Libft/Funciones_Libc/ft_split.c
@@ -14,5 +14,88 @@
 
 char	**ft_split(char *s, char c)
 {
-	
+	char	**res;
+	size_t	i;
+	int		j;
+	int		s_word;
+
+	i = 0;
+	j = 0;
+	s_word = 1;
+	res = ft_calloc((w_count(s, c) + 1), sizeof(char *));
+	if (!res)
+		return (NULL);
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && s_word < 0)
+		{
+			s_word = 1;
+		}
+		else if (s[i] == c || i == ft_strlen(s) || s_word >= 0)
+		{
+			res[j] = fill_word(s, s_word, i);
+			if (!(res[j]))
+				return (ft_free(res, j));
+			s_word = -1;
+			j++;
+		}
+		i++;
+	}
+	return (res);
+}
+
+static int	w_count(char *s, char c)
+{
+	int	count;
+	int id;
+
+	id = 0;
+	count = 0;
+	while (*s)
+	{
+		if (*s != c && id == 0)
+		{
+			count++;
+			id = 1;
+		}
+		 else if (*s == c)
+		 {
+			id = 0;
+		 }
+		s++;
+	}
+	return (count);
+}
+
+static char	*fill_word(const char *str, int start, int end)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc((end - start + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	while (start < end)
+	{
+		word[i] = str[start];
+		i++;
+		start++;
+	}
+	word[i] = 0;
+	return (word);
+}
+
+static void *ft_free(char **str, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+	return (NULL);
 }
