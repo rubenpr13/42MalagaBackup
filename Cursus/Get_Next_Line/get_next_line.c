ae336aa2db572b0f6f164979a93cf2b3af2441a4
rubenpr13 <rubenpinazorobles@gmail.com>
2024-09-13 13:52:00 +0200

    gnl fill_lines explain

--- Cursus/Get_Next_Line/get_next_line.c
+++ Cursus/Get_Next_Line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rpinazo- <rpinazo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 13:32:11 by rpinazo-          #+#    #+#             */
-/*   Updated: 2024/09/12 23:04:41 by rpinazo-         ###   ########.fr       */
+/*   Updated: 2024/09/13 13:51:15 by rpinazo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,13 @@ int	get_next_line(int fd)
 		buff = NULL;
 		return (NULL);
 	}
-
 	// contar lineas del archivo
 	num_lines = count_lines(fd);
-
 	//reservar memoria para cada línea 
 	// y gestión de errores
-	buff = malloc(num_lines * sizeof(char *));
+	buff = malloc(num_lines * sizeof(char *) + 1);
 	if (!buff)
 		return (NULL);
-	
 	// rellenar con las líneas
 	buff = fill_lines(fd, **buff, num_lines);
 	return (buff);
@@ -52,7 +49,6 @@ static int	count_lines(fd)
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	
 	// bucle para contar lineas del fd
 	while (read(fd, buffer,BUFFER_SIZE ) > 0)
 	{
@@ -69,9 +65,31 @@ static int	count_lines(fd)
 
 static char	**fill_lines(int fd, char **buff, int num_lines)
 {
-	int	i;
+	int		i;
+	int		x;
+	int		b;
+	char	*buffer;
 
-	
+	i = 0;
+	x = 0;
+	b = 0;
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	while (i <= num_lines)
+	{
+	// Contar cuantos bits necesita cada línea para reservar la memoria en buff[i][x]
+		while (read(fd, buffer, BUFFER_SIZE))
+		{
+			if (buffer[b] == '\n')
+
+		}
+	// reservar memoria para cada línea y gestionar fallos
+	// copiar la línea de buffer[b] a buff[i][x]
+	// escribir la línea
+	// liberar memoria de la línea
+	// pasar a la siguiente
+ 	}
 
 	return (buff);
 }
