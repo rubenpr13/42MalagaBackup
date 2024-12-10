eb1bcf3bf1eb0902b3f7336827cbf49c9811248e
rubenpr13 <rubenpinazorobles@gmail.com>
2024-12-10 14:28:14 +0100

    primera subida so_long

--- Cursus/so_long/src/main.c
+++ Cursus/so_long/src/main.c
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpinazo- <rpinazo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/19 10:46:32 by rpinazo-          #+#    #+#             */
-/*   Updated: 2024/04/19 14:16:08 by rpinazo-         ###   ########.fr       */
+/*   Created: 2024/12/10 13:58:59 by rpinazo-          #+#    #+#             */
+/*   Updated: 2024/12/10 14:25:41 by rpinazo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../lib/mlx.h"
+#include "../lib/libft.h"
 
-char	*ft_strrchr(const char *s, int c)
+int main ()
 {
-	char			mod_c;
-	char			*res;
-	unsigned int	i;
-
-	mod_c = (char) c;
-	res = NULL;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (mod_c == s[i])
-			res = ((char *) &s[i]);
-		i++;
-	}
-	if (s[i] == mod_c)
-		res = ((char *) &s[i]);
-	return (res);
+  
 }
