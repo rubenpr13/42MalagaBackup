13ca1a6a261d074a6707b013918839bc118c8415
rubenpr13 <rubenpinazorobles@gmail.com>
2024-12-12 10:08:15 +0100

    inicializacion de keys y definiciones

---
+++ Cursus/so_long/include/so_long.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpinazo- <rpinazo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/12 09:41:50 by rpinazo-          #+#    #+#             */
+/*   Updated: 2024/12/12 09:50:03 by rpinazo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../lib/mlx.h"
+# include "../lib/libft.h"
+
+// Definitions
+# define FLOOR      '0'
+# define WALL       '1'
+# define PLAYER     'P'
+# define COIN       'C'
+# define EXIT       'E'
+
+// Movements key ASCII
+# define KEY_W      119
+# define KEY_A      97
+# define KEY_S      115
+# define KEY_D      100
+
+# define KEY_UP     65362
+# define KEY_LEFT   65361
+# define KEY_RIGHT  65363
+# define KEY_DOWN   65354
+
+// Exit key  ASCII
+# define KEY_Q      113
+# define KEY_ESC    65307
+
+
+
+#endif
